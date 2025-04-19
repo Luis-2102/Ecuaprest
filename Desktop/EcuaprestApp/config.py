1125d7835f0cbe5fd6627f31490b61e52ac8af75
Luis-2102 <lalomoto5990@utm.edu.ec>
2025-04-18 20:02:18 -0500

    Actualizacion login

--- Desktop/EcuaprestApp/config.py
+++ Desktop/EcuaprestApp/config.py
@@ -2,6 +2,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost:5432/Ecuaprest'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+#    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Luis175365.@localhost:5433/Ecuaprest'
+#    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
